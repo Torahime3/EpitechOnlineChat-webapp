1257839b33ebab690b876a66481d0fb5e5b67641
TalalBoni <talal.bounaama@epitech.eu>
2024-01-23 11:11:10 +0100

    add: BackEnd

---
+++ Backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const PORT = 3000
+const HOST = "0.0.0.0"
+
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('Test The Best HEHEHE')
+})
+
+app.listen(PORT, HOST, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
