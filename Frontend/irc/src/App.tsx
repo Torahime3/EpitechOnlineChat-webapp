29b6b67ebf9d3ee841494f148c36e643d5b5641d
Nathan Dulac <nathan.dulac@epitech.eu>
2024-01-23 11:03:28 +0100

    feat(Frontend)

---
+++ Frontend/irc/src/App.tsx
@@ -0,0 +1,8 @@
+import Hello from "./components/Hello.tsx";
+
+function App() {
+
+    return <div><Hello/></div>
+
+}
+export default App
---
+++ Frontend/irc/src/components/Hello.tsx
@@ -0,0 +1,8 @@
+
+function Hello(){
+
+    const name = "world";
+    return <h1> Hello {name} </h1>;
+
+}
+export default Hello;
