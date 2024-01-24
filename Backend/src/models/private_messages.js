f4b2cf5e50d611400e752bb0e9633d1c76226004
TalalBoni <talal.bounaama@epitech.eu>
2024-01-24 23:58:52 +0100

    add: ModelsPrivateMessage

---
+++ Backend/src/models/private_messages.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const privateMessageSchema = new mongoose.Schema({
+    user_id: {
+        type: Number,
+        ref: 'users',
+        required: true
+    },
+    pm_message_content: {
+        type: String,
+        required: true,
+        maxlength: 500
+    },
+    pm_message_date: {
+        type: Date,
+        required: true
+    },
+    target_id: {
+        type: Number,
+        ref: 'users',
+        required: true
+    },
+});
+
+const PrivateMessage = mongoose.model('privatemessage', privateMessageSchema)
+module.exports = PrivateMessage;
