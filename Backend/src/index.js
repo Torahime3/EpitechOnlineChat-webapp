684d052169c8bb7dbe6426893fd43859519185aa
TalalBoni <talal.bounaama@epitech.eu>
2024-01-31 01:27:47 +0100

    update main

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,11 +6,8 @@ const HOST = "0.0.0.0"
 
 //Routes
 const userRoutes = require('./routes/usersRoutes');
-<<<<<<< HEAD
 const channelRoute = require('./routes/channelsRoutes')
-=======
 const privateMessageRoutes = require('./routes/privateMessageRoutes');
->>>>>>> c01774508224b7858b2d675e9f407a5562e5ee0e
 
 const app = express()
 mongoose.connect("mongodb+srv://bounaamatalal:Retrouver64@cluster0.1hou5pg.mongodb.net/t-jsf?retryWrites=true&w=majority")
@@ -27,11 +24,8 @@ app.use((req, res, next) => {
 
 //On dit à express d'utiliser les routes définies dans le fichier usersRoutes.js
 app.use('/api/v1/users', userRoutes);
-<<<<<<< HEAD
 app.use('/api/v1/channels', channelRoute);
-=======
 app.use('/api/v1/privateMessages', privateMessageRoutes);
->>>>>>> c01774508224b7858b2d675e9f407a5562e5ee0e
 
 app.listen(PORT, HOST, () => {
   console.log(`Example app listening on port ${PORT}`)
